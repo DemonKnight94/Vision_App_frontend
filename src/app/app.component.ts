fc77fbb19a90ed0489236077a656589b0bb629d9
DemonKnight94 <devilsgrin94@gmail.com>
2021-03-02 17:48:32 +0530

    End of ng frotend part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-ng-app';
+export class AppComponent implements OnInit {
+  title = 'My ui sample app';
+  users: any;
+  stringifiedData: any;
+  parsedJson: any;
+
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    const obs1=this.http.get('https://localhost:5001/api/users');
+    obs1.subscribe((response)=>
+    {
+      this.users=response;
+      console.log(response);
+
+    })
+  }
+
+  ngOnInit() {
+
+    this.getUsers();
+
+  }
+
+  ngOnDestroy(){
+    this.users.unsubscribe();
+  }
 }
